07aa382fad12272905fdca7f38a702c562548eaf
Bardia Moshiri <bardia@furilabs.com>
2024-10-03 23:41:36 -0400

    plugins: location-privacy-quick-setting: add finalize and use g_signal_connect_swapped instead
    also some small cleanups to be more phosh styled
    
    Signed-off-by: Bardia Moshiri <bardia@furilabs.com>

--- plugins/location-privacy-quick-setting/location-privacy-quick-setting.c
+++ plugins/location-privacy-quick-setting/location-privacy-quick-setting.c
@@ -20,8 +20,8 @@
 struct _PhoshLocationPrivacyQuickSetting {
   PhoshQuickSetting        parent;
 
-  PhoshStatusIcon         *info;
   GSettings               *settings;
+  PhoshStatusIcon         *info;
 };
 
 G_DEFINE_TYPE (PhoshLocationPrivacyQuickSetting, phosh_location_privacy_quick_setting, PHOSH_TYPE_QUICK_SETTING);
@@ -54,16 +54,32 @@ on_clicked (PhoshLocationPrivacyQuickSetting *self)
 }
 
 static void
-on_settings_changed (GSettings *settings, gchar *key, PhoshLocationPrivacyQuickSetting *self)
+on_settings_changed (PhoshLocationPrivacyQuickSetting *self,
+                     gchar                            *key,
+                     GSettings                        *_settings)
 {
   if (g_strcmp0 (key, "enabled") == 0)
     update_location_privacy_status (self);
 }
 
 static void
+phosh_location_finalize (GObject *object)
+{
+  PhoshLocationPrivacyQuickSetting *self = PHOSH_LOCATION_PRIVACY_QUICK_SETTING (object);
+
+  g_clear_object (&self->settings);
+
+  G_OBJECT_CLASS (phosh_location_privacy_quick_setting_parent_class)->finalize (object);
+}
+
+
+static void
 phosh_location_privacy_quick_setting_class_init (PhoshLocationPrivacyQuickSettingClass *klass)
 {
+  GObjectClass *object_class = (GObjectClass *)klass;
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->finalize = phosh_location_finalize;
 
   gtk_widget_class_set_template_from_resource (widget_class,
                                                "/mobi/phosh/plugins/location-privacy-quick-setting/qs.ui");
@@ -76,10 +92,12 @@ phosh_location_privacy_quick_setting_class_init (PhoshLocationPrivacyQuickSettin
 static void
 phosh_location_privacy_quick_setting_init (PhoshLocationPrivacyQuickSetting *self)
 {
-  self->settings = g_settings_new ("org.gnome.system.location");
   gtk_widget_init_template (GTK_WIDGET (self));
 
-  g_signal_connect (self->settings, "changed::enabled", G_CALLBACK (on_settings_changed), self);
+  self->settings = g_settings_new ("org.gnome.system.location");
 
   update_location_privacy_status (self);
+
+  g_signal_connect_swapped (self->settings, "changed::enabled",
+                            (GCallback) on_settings_changed, self);
 }
